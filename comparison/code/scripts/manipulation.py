f74f661e44b5ceff2e4f756455357effb551fc70
keithgraham5 <keith.graham5@nhs.net>
2023-10-04 19:32:03 +0100

    Refactor Manipulation Sctipt
    Removed unused imports and code.
    Refactored the script to use functions for normalising chromosome lenghts, scaling data, and transofming/sotring data.
    Improved code reasbility and maintainability.
    Updated the main function to use these fucntions and return the normalised lengths and tansformed data.
    Removed unnecessary class defintions.

--- comparison/code/scripts/manipulation.py
+++ comparison/code/scripts/manipulation.py
@@ -3,10 +3,7 @@ import mplcursors  # Import mplcursors
 from decimal import Decimal
 import matplotlib.cm as cm  # Import the color map module
 
-from extraction import data_sources
 
-# Create a figure and axis
-fig, ax = plt.subplots()
 
 # Define chromosome lengths (you can customize this based on your data)
 chromosome_lengths = {
@@ -36,7 +33,8 @@ chromosome_lengths = {
     "chrY": (1, 59373566),
 }
 
-# Sample data for different datasets
+
+# # Sample data for different datasets
 # data_sources = [
 #     (
 #         "call_data",
@@ -74,158 +72,120 @@ chromosome_mapping = {
 }
 
 
-class ChromosomeLengthNormaliser:
-    def __init__(self, chromosome_lengths):
-        """
-        Initialize a ChromosomeLengthNormaliser object.
+def normalise_chromosome_lengths(chromosome_lengths):
+    """
+    Normalize chromosome lengths to whole numbers.
 
-        Args:
-            chromosome_lengths (dict): A dictionary containing chromosome lengths.
-        """
-        self.chromosome_lengths = chromosome_lengths
-        self.normalised_chromosome_lengths = {}
-        self.scaling_factors = {}
+    Args:
+        chromosome_lengths (dict): A dictionary containing chromosome lengths.
 
-    def normalise_chromosome_lengths(self):
-        """
-        Normalize each chromosome's length to a whole number.
+    Returns:
+        tuple: A tuple containing two dictionaries: normalised_chromosome_lengths
+        and scaling_factors.
+    """
+    normalised_chromosome_lengths = {}
+    scaling_factors = {}
+    
 
-        Args:
-            chromosome_lengths (dict): A dictionary containing chromosome lengths.
+    # Loop through each chromosome and its corresponding start and end positions
+    for i, (chromosome, (start, end)) in enumerate(chromosome_lengths.items(), start=1):
+        # Calculate the length of the chromosome
+        chromosome_length = end - start
 
-        Returns:
-            tuple: A tuple containing two dictionaries: normalised_chromosome_lengths
-            and scaling_factors.
-        """
+        # Calculate the scaling factor for this chromosome (1 divided by chromosome length)
+        scaling_factor = Decimal(1) / Decimal(chromosome_length)
 
-        # Loop through each chromosome and its corresponding start and end positions
-        for i, (chromosome, (start, end)) in enumerate(
-            self.chromosome_lengths.items(), start=1
-        ):
+        # Calculate the scaled start position based on the current index 'i'
+        scaled_start = Decimal(i)
 
-            # Calculate the length of the chromosome
-            chromosome_length = end - start
+        # Calculate the scaled end position based on the scaling factor
+        scaled_end = scaled_start + Decimal(chromosome_length) * scaling_factor
 
-            # Calculate the scaling factor for this chromosome (1 divided by chromosome length)
-            scaling_factor = Decimal(1) / Decimal(chromosome_length)
+        # Store the normalized chromosome lengths in a dictionary
+        normalised_chromosome_lengths[chromosome] = (scaled_start, scaled_end)
 
-            # Calculate the scaled start position based on the current index 'i'
-            scaled_start = Decimal(i)
+        # Store the scaling factors for later reference
+        scaling_factors[chromosome] = scaling_factor
+        
 
-            # Calculate the scaled end position based on the scaling factor
-            scaled_end = scaled_start + Decimal(chromosome_length) * scaling_factor
+    return normalised_chromosome_lengths, scaling_factors
+    
 
-            # Store the normalized chromosome lengths in a dictionary
-            self.normalised_chromosome_lengths[chromosome] = (
-                scaled_start,
-                scaled_end,
+def scale_data(data, scaling_factors):
+    """
+    Scale the data using provided scaling factors.
+
+    Args:
+        data (list): A list of tuples containing data to be scaled.
+        scaling_factors (dict): A dictionary containing scaling factors for each chromosome.
+
+    Returns:
+        list: A list of scaled data.
+    """
+    scaled_data = []
+
+    for source_name, source_data in data:
+        scaled_source_data = []  # Create a list for storing scaled data
+        for chromosome, (start, end, value) in source_data:
+            # Get scaling factor for this chromosome from the provided dictionary
+            scaling_factor = scaling_factors[chromosome]
+
+            # Scale the start and end positions using the scaling factor
+            scaled_start = Decimal(start) * scaling_factor
+            scaled_end = Decimal(end) * scaling_factor
+
+            # Append the scaled data to the list
+            scaled_source_data.append((chromosome, (scaled_start, scaled_end, value)))
+
+        # Append the scaled source data to the list of scaled data
+        scaled_data.append((source_name, scaled_source_data))
+
+    return scaled_data
+
+
+
+def transform_and_store_data(scaled_data, chromosome_mapping):
+    """
+    Transform and store the scaled sample data.
+
+    Args:
+        scaled_data (list): A list of tuples containing the scaled data.
+        chromosome_mapping (dict): A dictionary mapping chromosome names to their transformed values.
+
+    Returns:
+        list: A list containing tuples of transformed and stored data.
+    """
+    transformed_scaled_data = []
+
+    for source_name, source_data in scaled_data:
+        transformed_source_data = []  # Create a list for storing transformed data
+        for chromosome, (start, end, call) in source_data:
+            # Calculate transformed start and end positions based on chromosome mapping
+            transformed_start = Decimal(start) + Decimal(
+                chromosome_mapping.get(chromosome, chromosome[3:])
+            )
+            transformed_end = Decimal(end) + Decimal(
+                chromosome_mapping.get(chromosome, chromosome[3:])
             )
 
-            # Store the scaling factors for later reference
-            self.scaling_factors[chromosome] = scaling_factor
+            # Append the transformed data to the list
+            transformed_source_data.append(
+                (chromosome, (transformed_start, transformed_end, call))
+            )
+
+        # Append the transformed source data to the list of transformed data
+        transformed_scaled_data.append((source_name, transformed_source_data))
+    print(transformed_scaled_data)
+
+    return transformed_scaled_data
 
 
-class DataScaler:
-    """
-    A class for scaling data using provided scaling factors.
-    """
-
-    def __init__(self, data, scaling_factors):
-        """
-        Initialise a DataScaler object.
-
-        Args:
-            data (list): A list of tuples containing data to be scaled.
-            scaling_factors (dict): A dictionary containing scaling factors for each chromosome.
-        """
-        self.data = data
-        self.scaling_factors = scaling_factors
-        self.scaled_data = []
-
-    def scale_data(self):
-        """
-        Scale the data using provided scaling factors.
-
-        This method scales the start and end positions of data for each chromosome
-        based on the corresponding scaling factor and stores the scaled data.
-
-        Returns:
-            None
-        """
-        for source_name, source_data in self.data:
-            scaled_source_data = []  # Create a list for storing scaled data
-            for chromosome, (start, end, value) in source_data:
-                # Get scaling factor for this chromosome from provided dictionary
-                scaling_factor = self.scaling_factors[chromosome]
-
-                # Scale the start and end positions using the scaling factor
-                scaled_start = Decimal(start) * scaling_factor
-                scaled_end = Decimal(end) * scaling_factor
-
-                # Append the scaled data to the list
-                scaled_source_data.append((chromosome, (scaled_start, scaled_end, value)))
-
-            # Append the scaled source data to the list of scaled data
-            self.scaled_data.append((source_name, scaled_source_data))
+def manipulate_data(filtered_parsed_data):
+    normalised_lengths, scaling_factors = normalise_chromosome_lengths(chromosome_lengths)
+    scaled_data = scale_data(filtered_parsed_data, scaling_factors)
+    transformed_data = transform_and_store_data(scaled_data, chromosome_mapping)
+    
+    return normalised_lengths, transformed_data
 
 
-class DataTransformer:
-    """
-    A class for transforming and storing scaled data using chromosome mapping.
-    """
 
-    def __init__(self, scaled_data, chromosome_mapping):
-        """
-        Initialize a DataTransformer object.
-
-        Args:
-            scaled_data (list): A list of tuples containing the scaled data.
-            chromosome_mapping (dict): A dictionary mapping chromosome names to their transformed values.
-        """
-        self.scaled_data = scaled_data
-        self.chromosome_mapping = chromosome_mapping
-        self.transformed_scaled_data = []
-
-    def transform_and_store_data(self):
-        """
-        Transform and store the scaled sample data.
-
-        This method takes the scaled data and applies a transformation based on the provided
-        chromosome mapping. It then stores the transformed data.
-
-        Returns:
-            list: A list containing tuples of transformed and stored data.
-        """
-        for source_name, source_data in self.scaled_data:
-            transformed_source_data = []  # Create a list for storing transformed data
-            for chromosome, (start, end, call) in source_data:
-                # Calculate transformed start and end positions based on chromosome mapping
-                transformed_start = Decimal(start) + Decimal(
-                    self.chromosome_mapping.get(chromosome, chromosome[3:])
-                )
-                transformed_end = Decimal(end) + Decimal(
-                    self.chromosome_mapping.get(chromosome, chromosome[3:])
-                )
-
-                # Append the transformed data to the list
-                transformed_source_data.append(
-                    (chromosome, (transformed_start, transformed_end, call))
-                )
-
-            # Append the transformed source data to the list of transformed data
-            self.transformed_scaled_data.append((source_name, transformed_source_data))
-
-# Create instances of the classes
-normaliser = ChromosomeLengthNormaliser(chromosome_lengths)
-scaler = DataScaler(data_sources, normaliser.scaling_factors)
-transformer = DataTransformer(scaler.scaled_data, chromosome_mapping)
-
-# Normalize chromosome lengths
-normaliser.normalise_chromosome_lengths()
-# Scale the call data
-scaler.scale_data()
-# Transform and store the scaled data
-transformer.transform_and_store_data()
-
-
-# Sanity checks
