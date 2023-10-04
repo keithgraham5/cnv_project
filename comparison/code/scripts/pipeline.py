c07514884db2b12c1922e1df19bb983c0e71e69f
keithgraham5 <keith.graham5@nhs.net>
2023-10-04 19:24:10 +0100

    Refactor Pipeline Script
    Removed directi imports of data_sources from the 'extration' and 'manipulation' modules.
    Updated extraction process to use the 'extract_data' function from 'extraction' module.
    Updated data manipulation process to use the 'manipualte_data' fucntion from manipulation modules, passing the filtered_parsed_data as arguments.
    Enabled the plotting process by calling 'build_and_plot_graph' fucntion from the 'plotting' modules with appropriate arguments.
    Maintained clear speration of concerns and aovided module coupling by passing argments when calling scripts.
    
    These changes enahance modularity and maintainabilty of the codebase.

--- comparison/code/scripts/pipeline.py
+++ comparison/code/scripts/pipeline.py
@@ -3,10 +3,14 @@ import manipulation
 import plotting
 
 # Run the data extraction process
-data_sources = extraction.data_sources
+filtered_parsed_data = extraction.extract_data()
 
 
 # manipulation.manipulate_data()
+normalised_lengths, transformed_scaled_data, = manipulation.manipulate_data(filtered_parsed_data)
+print(normalised_lengths)
+print(transformed_scaled_data)
 
-# # Run the plotting process
-# plotting.build_and_plot_graph()
+
+# Run the plotting process
+graph = plotting.build_and_plot_graph(normalised_lengths, transformed_scaled_data)
