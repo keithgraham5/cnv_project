d50caf3ece56ec1340fc0d6da6152ddbe8251d67
keithgraham5 <keith.graham5@nhs.net>
2023-10-05 19:39:42 +0100

    WIP

--- comparison/code/scripts/sensitivity.py
+++ comparison/code/scripts/sensitivity.py
@@ -1,32 +1,75 @@
-import pandas as pd 
+import pandas as pd
 
 
+# Define the filepaths for the data
+# snp_data = "/home/keith/BIOL61860_research_project_in_clinical_bioinformatics/cnv_project/comparison/data/test_data/test_snp_output.txt"
+# regions_of_interest_data = "~/BIOL61860_research_project_in_clinical_bioinformatics/cnv_project/comparison/data/reference_data/20220207_PanHaemOnc.bed"
 
-snp_data = "/home/keith/BIOL61860_research_project_in_clinical_bioinformatics/cnv_project/comparison/data/test_data/test_snp_output.txt"
-regions_of_interesa_data = "~/BIOL61860_research_project_in_clinical_bioinformatics/cnv_project/comparison/data/reference_data/20220207_PanHaemOnc.bed"
+# Define the filepaths for the test data
+snp_data = "/home/keith/BIOL61860_research_project_in_clinical_bioinformatics/cnv_project/comparison/data/test_data/shorter_test_snp_output.txt"
+regions_of_interest_data = "~/BIOL61860_research_project_in_clinical_bioinformatics/cnv_project/comparison/data/test_data/shorter_20220207_PanHaemOnc.bed"
 
 
+# Define the mapping for renaming CHROMOSOME values
+chromosome_mapping = {
+    'chrX': 'chr23',
+    'chrY': 'chr24'
+}
 
+# Read the PanHaemOnc.bed file
+regions_of_interest = pd.read_csv(regions_of_interest_data, delimiter="\t")
 
-df = pd.read_csv(snp_data, delimiter="\t")
-column_headers = df.columns
-df['NEW_CHROMOSOME'] = 'chr' +df['CHROMOSOME'].astype(str)
-print(f" snp_data columns header:\n {column_headers}")
-print(df.iloc[0])
-
+# Define column names for regions of interest
 column_names = ['CHROMOSOME', 'START', 'END', 'GENE']
-df = pd.read_csv(regions_of_interesa_data, delimiter="\t")
-df = pd.DataFrame(df)
-df.columns = column_names
-column_headers = df.columns 
-print(f"PanHeamOnc.bed file columns header:\n {column_headers}")
-print(df.iloc[0])
 
-# Initialise an empty list to store filtered SNP data
-# Loop through each row in the PanHaemOnc.bed file
-# Filter SNP data based on matching 'NEW_CHROMOSOME'
-# Filter SNP data further based on 'POSITION' within 'START' and 'END'
-# Append filtered rows to the result
-# Concatenate all the filtered SNP data rows into a single DataFrame
-# Now, filtered_snp_data_df contains SNP data that falls within the regions of interest
+# Rename the columns in regions_of_interest
+regions_of_interest.columns = column_names
 
+# Rename the 'CHROMOSOME' values based on the mapping
+regions_of_interest['CHROMOSOME'] = regions_of_interest['CHROMOSOME'].replace(chromosome_mapping)
+
+
+# Read the snp_data file
+snp_data = pd.read_csv(snp_data, delimiter="\t")
+
+# Get the column headers for snp_data
+column_headers = snp_data.columns
+
+# Create a new 'NEW_CHROMOSOME' column by prefixing 'chr' to 'CHROMOSOME' values
+snp_data['NEW_CHROMOSOME'] = 'chr' + snp_data['CHROMOSOME'].astype(str)
+
+# Merge the DataFrames based on the 'NEW_CHROMOSOME' column
+merged_data = snp_data.merge(regions_of_interest, left_on='NEW_CHROMOSOME', right_on='CHROMOSOME')
+
+# Filter rows where 'POSITION' is within the 'START' and 'END' range
+filtered_data = merged_data[(merged_data['POSITION'] >= merged_data['START']) & (merged_data['POSITION'] <= merged_data['END'])]
+
+# Filter rows in filtered_data where 'TYPE' is not 'NO CHANGE'
+filtered_data_not_no_change = filtered_data[filtered_data['TYPE'] != 'NO CHANGE']
+
+# Print the filtered data without 'NO CHANGE' in the 'TYPE' column
+print(filtered_data_not_no_change)
+
+# Calculate the number of included and discarded probes
+included_probes = len(filtered_data)
+discarded_probes = len(snp_data) - included_probes
+
+# Print the results
+print(filtered_data)
+print(f"Probes included: {included_probes}")
+print(f"Probes discarded: {discarded_probes}")
+
+cnvkit_data = "/home/keith/BIOL61860_research_project_in_clinical_bioinformatics/cnv_project/comparison/data/test_data/H28310-21.sorted.dedup.recalibrated.Tumor.call.cns"
+
+# cnvkit_data = "/home/keith/BIOL61860_research_project_in_clinical_bioinformatics/cnv_project/comparison/data/test_datatest_H28194-21.sorted.dedup.recalibrated.Tumor.call.cns"
+
+# Read the file into a DataFrame
+df = pd.read_csv(cnvkit_data, delimiter="\t")
+
+# Now, 'df' contains the data from the file as a DataFrame   
+print(df.columns)
+
+
+num_rows = df.shape[0]
+print(f"Number of rows: {num_rows}")
+        
